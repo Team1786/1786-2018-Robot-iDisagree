cdaa3b9fe48b64ba93fde6350cd8196fbb907c5e
Phillip Simons <p_simons@dublinschool.org>
2018-01-19 11:29:35 -0500

    Setup talons and joysticks. Added arcadeDrive with a deadband of .05

--- 2018 Robot iDisagree/src/org/usfirst/frc/team1786/robot/Robot.java
+++ 2018 Robot iDisagree/src/org/usfirst/frc/team1786/robot/Robot.java
@@ -10,6 +10,10 @@ package org.usfirst.frc.team1786.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -24,6 +28,21 @@ public class Robot extends IterativeRobot {
 	private String m_autoSelected;
 	private SendableChooser<String> m_chooser = new SendableChooser<>();
 
+
+	
+	
+	Joystick joystickLeft = new Joystick(0);
+	Joystick joystickRight = new Joystick(1);
+	
+	WPI_TalonSRX talonL1 = new WPI_TalonSRX(1);
+	WPI_TalonSRX talonL2 = new WPI_TalonSRX(2);
+	WPI_TalonSRX talonL3 = new WPI_TalonSRX(3);
+	WPI_TalonSRX talonR4 = new WPI_TalonSRX(4);
+	WPI_TalonSRX talonR5 = new WPI_TalonSRX(5);
+	WPI_TalonSRX talonR6 = new WPI_TalonSRX(6);
+	
+	DifferentialDrive myRobot = new DifferentialDrive(talonL1, talonR4);
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -33,6 +52,18 @@ public class Robot extends IterativeRobot {
 		m_chooser.addDefault("Default Auto", kDefaultAuto);
 		m_chooser.addObject("My Auto", kCustomAuto);
 		SmartDashboard.putData("Auto choices", m_chooser);
+		
+		talonL2.follow(talonL1);
+		talonL3.follow(talonL1);
+		talonR5.follow(talonR4);
+		talonR6.follow(talonR4);
+		
+		double deadband = 0.05;
+		
+		myRobot.setDeadband(deadband);
+
+		
+		
 	}
 
 	/**
@@ -75,6 +106,14 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
+		
+		
+		
+		myRobot.arcadeDrive(joystickLeft.getY(), joystickLeft.getZ(), true);
+		
+		
+		
+		
 	}
 
 	/**
