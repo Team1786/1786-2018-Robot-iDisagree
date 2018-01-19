5c42e1d8d1ec3a7d08b5c636bb983b5924d8ab00
colinbrown <colinbrown@192.168.20.36>
2018-01-19 11:46:28 -0500

    I wrote code that worked to drive the robot forward, backwards, and for it to turn.

--- 2018 Robot iDisagree/src/org/usfirst/frc/team1786/robot/Robot.java
+++ 2018 Robot iDisagree/src/org/usfirst/frc/team1786/robot/Robot.java
@@ -8,8 +8,11 @@
 package org.usfirst.frc.team1786.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.drive.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,7 +26,19 @@ public class Robot extends IterativeRobot {
 	private static final String kCustomAuto = "My Auto";
 	private String m_autoSelected;
 	private SendableChooser<String> m_chooser = new SendableChooser<>();
+	
+	WPI_TalonSRX talonL1 = new WPI_TalonSRX(1);
+	WPI_TalonSRX talonL2 = new WPI_TalonSRX(2);
+	//WPI_TalonSRX talonL3 = new WPI_TalonSRX(3);
+	WPI_TalonSRX talonR4 = new WPI_TalonSRX(3);
+	WPI_TalonSRX talonR5 = new WPI_TalonSRX(4);
+	//WPI_TalonSRX talonR6 = new WPI_TalonSRX(6);
+	
+	DifferentialDrive myRobot = new DifferentialDrive(talonL1, talonR4);
 
+	Joystick joystickRight = new Joystick(1); //set ID 1 in DriverStation
+	Joystick joystickLeft = new Joystick(0); //set ID 2 in Driver Station
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -33,6 +48,15 @@ public class Robot extends IterativeRobot {
 		m_chooser.addDefault("Default Auto", kDefaultAuto);
 		m_chooser.addObject("My Auto", kCustomAuto);
 		SmartDashboard.putData("Auto choices", m_chooser);
+		
+		talonL2.follow(talonL1);
+		//talonL3.follow(talonL1);
+		talonR5.follow(talonR4);
+		//talonR6.follow(talonR4);
+		
+		
+		
+		
 	}
 
 	/**
@@ -75,6 +99,9 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
+		
+		myRobot.arcadeDrive(joystickLeft.getY(), joystickLeft.getZ(), true);
+		
 	}
 
 	/**
