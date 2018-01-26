4743e515b3c4ad45f5c988c2e47ebaaa4cd87926
reed2327 <33269766+reed2327@users.noreply.github.com>
2018-01-26 13:23:42 -0500

    setup for solnoid and commpressor plus set up forshifting buttons

--- 2018 Robot iDisagree/src/org/usfirst/frc/team1786/robot/Robot.java
+++ 2018 Robot iDisagree/src/org/usfirst/frc/team1786/robot/Robot.java
@@ -14,6 +14,11 @@ import edu.wpi.first.wpilibj.Joystick;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -22,6 +27,12 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
  * project.
  */
 public class Robot extends IterativeRobot {
+	private int currentLimit1 = 40;
+	private int currentLimit2 = 60;
+	private int currentDur = 10000;
+	private int upShiftButtonNum = 5;
+	private int downShiftButtonNum = 6;
+	
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
 	private String m_autoSelected;
@@ -29,7 +40,8 @@ public class Robot extends IterativeRobot {
 
 	Joystick joystickLeft = new Joystick(0);
 	Joystick joystickRight = new  Joystick(1);
-	
+	JoystickButton upshiftbutton = new JoystickButton(joystickLeft,upShiftButtonNum);
+	JoystickButton downshiftbutton = new JoystickButton(joystickLeft,downShiftButtonNum);
 	
 	WPI_TalonSRX talonL1 = new WPI_TalonSRX(1);
 	WPI_TalonSRX talonL2 = new WPI_TalonSRX(2);
@@ -37,6 +49,9 @@ public class Robot extends IterativeRobot {
 	WPI_TalonSRX talonR4 = new WPI_TalonSRX(3);
 	WPI_TalonSRX talonR5 = new WPI_TalonSRX(4);
 	//WPI_TalonSRX talonR6 = new WPI_TalonSRX(6);
+	
+	Compressor compressor1 = new Compressor(1);
+	Solenoid solenoid1 = new Solenoid(1);
 	
 	DifferentialDrive myRobot = new DifferentialDrive (talonL1, talonR4);
 	/**
@@ -48,10 +63,45 @@ public class Robot extends IterativeRobot {
 		m_chooser.addDefault("Default Auto", kDefaultAuto);
 		m_chooser.addObject("My Auto", kCustomAuto);
 		SmartDashboard.putData("Auto choices", m_chooser);
-	talonL2.follow(talonL1);
-	//talonL3.follow(talonL1);
-	talonR5.follow(talonR4);
-	//talonR6.follow(talonR4);
+		
+		//limiting the current all the time
+		talonL1.configContinuousCurrentLimit(currentLimit1,0);
+		talonL2.configContinuousCurrentLimit(currentLimit1,0);
+		//talonL3.configContinuousCurrentLimit(currentLimit1,0);
+		talonR4.configContinuousCurrentLimit(currentLimit1,0);
+		talonR5.configContinuousCurrentLimit(currentLimit1,0);
+		//talonR6.configContinuousCurrentLimit(currentLimit1,0);
+		
+		//time when peak is active
+		talonL1.configPeakCurrentDuration(currentDur,0);
+		talonL2.configPeakCurrentDuration(currentDur,0);
+		//talonL3.configPeakCurrentDuration(currentDur,0);
+		talonR4.configPeakCurrentDuration(currentDur,0);
+		talonR5.configPeakCurrentDuration(currentDur,0);
+		//talonR6.configPeakCurrentDuration(currentDur,0);
+		
+		//only when the above is active
+		talonL1.configPeakCurrentLimit(currentLimit2,0);
+		talonL2.configPeakCurrentLimit(currentLimit2,0);
+		//talonL3.configPeakCurrentLimit(currentLimit2,0);
+		talonR4.configPeakCurrentLimit(currentLimit2,0);
+		talonR5.configPeakCurrentLimit(currentLimit2,0);
+		//talonR6.configPeakCurrentLimit(currentLimit2,0);
+		
+		//does it do what it do
+		talonL1.enableCurrentLimit(true);
+		talonL2.enableCurrentLimit(true);
+		//talonL3.enableCurrentLimit(true);
+		talonR4.enableCurrentLimit(true);
+		talonR5.enableCurrentLimit(true);
+		//talonR6.enableCurrentLimit(true);
+		
+		talonL2.follow(talonL1);
+		//talonL3.follow(talonL1);
+		talonR5.follow(talonR4);
+		//talonR6.follow(talonR4);
+	
+	
 	}
 	
 	/**
@@ -96,7 +146,28 @@ public class Robot extends IterativeRobot {
 	public void teleopPeriodic() {
 		SmartDashboard.putNumber("joystickY", joystickLeft.getY());
 		SmartDashboard.putNumber("joystickZ", joystickLeft.getZ());
-		myRobot.arcadeDrive(-joystickLeft.getY(), -joystickLeft.getZ(), false);
+		
+		//the tempature
+		SmartDashboard.putNumber("talonL1Temp", talonL1.getTemperature());
+		SmartDashboard.putNumber("talonL2Temp", talonL2.getTemperature());
+		//SmartDashboard.putNumber("talonL3Temp", talonL3.getTemperature());
+		SmartDashboard.putNumber("talonR3Temp", talonR4.getTemperature());
+		SmartDashboard.putNumber("talonR5Temp", talonR5.getTemperature());
+		//SmartDashboard.putNumber("talonR6Temp", talonR6.getTemperature());
+		
+		//the output current
+		SmartDashboard.putNumber("talonL1Current", talonL1.getOutputCurrent());
+		SmartDashboard.putNumber("talonL2TCurrent", talonL2.getOutputCurrent());
+		//SmartDashboard.putNumber("talonL3Current", talonL3.getOutputCurrent());
+		SmartDashboard.putNumber("talonR3Current", talonR4.getOutputCurrent());
+		SmartDashboard.putNumber("talonR5Current", talonR5.getOutputCurrent());
+		//SmartDashboard.putNumber("talonR6Current", talonR6.getOutputCurrent());
+		
+		
+		
+		myRobot.arcadeDrive(-joystickLeft.getY(), joystickLeft.getZ(), false);
+		
+	
 	}
 	
 	
